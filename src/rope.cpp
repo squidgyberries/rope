674d494c77a48dc940796ea2c60245c25b1af895
squidgyberries <43133000+squidgyberries@users.noreply.github.com>
2023-02-18 21:24:24 +0800

    allow mouse movement for locked points

--- src/rope.cpp
+++ src/rope.cpp
@@ -10,18 +10,24 @@
 constexpr int window_width = 800;
 constexpr int window_height = 600;
 
+constexpr float link_width = 12.0f;
+constexpr float point_radius = 18.0f;
+
 constexpr int font_size = 20;
 
 size_t orig_index = 0;
 
-std::vector<Point> points;
-std::vector<Link> links;
+std::vector<Point> points = {};
+std::vector<Link> links = {};
 
 std::mutex points_links_mutex;
 
 std::atomic<bool> should_close = false;
-std::atomic<float> physics_delta_time;
+std::atomic<float> physics_delta_time = 0.0f;
 std::atomic<bool> paused = true;
+
+Point *grabbed_point = nullptr;
+Vector2 grabbed_offset = vec2(0.0f, 0.0f);
 
 constexpr int num_iterations = 5;
 
@@ -55,22 +61,18 @@ void simulate() {
             Vector2 offset =
                 Vector2Subtract(points[link.ib].pos, points[link.ia].pos);
             Vector2 new_offset = Vector2Scale(offset, scale);
-            points[link.ib].pos =
-                Vector2Add(points[link.ia].pos, new_offset);
+            points[link.ib].pos = Vector2Add(points[link.ia].pos, new_offset);
           } else if (points[link.ib].state != Point::State::Free) {
             Vector2 offset =
                 Vector2Subtract(points[link.ia].pos, points[link.ib].pos);
             Vector2 new_offset = Vector2Scale(offset, scale);
-            points[link.ia].pos =
-                Vector2Add(points[link.ib].pos, new_offset);
-          } else if (points[link.ia].state == Point::State::Free && points[link.ib].state == Point::State::Free) {
-            Vector2 sum =
-                Vector2Add(points[link.ia].pos, points[link.ib].pos);
+            points[link.ia].pos = Vector2Add(points[link.ib].pos, new_offset);
+          } else if (points[link.ia].state == Point::State::Free &&
+                     points[link.ib].state == Point::State::Free) {
+            Vector2 sum = Vector2Add(points[link.ia].pos, points[link.ib].pos);
             Vector2 center = Vector2Multiply(sum, vec2(0.5f, 0.5f));
-            Vector2 offset_a =
-                Vector2Subtract(points[link.ia].pos, center);
-            Vector2 offset_b =
-                Vector2Subtract(points[link.ib].pos, center);
+            Vector2 offset_a = Vector2Subtract(points[link.ia].pos, center);
+            Vector2 offset_b = Vector2Subtract(points[link.ib].pos, center);
             Vector2 new_offset_a = Vector2Scale(offset_a, scale);
             Vector2 new_offset_b = Vector2Scale(offset_b, scale);
             points[link.ia].pos = Vector2Add(center, new_offset_a);
@@ -137,49 +139,68 @@ int main() {
       }
     }
 
+    points_links_mutex.lock();
+
+    for (Point &point : points) {
+      if (point.state == Point::State::Locked &&
+          IsMouseButtonPressed(MOUSE_BUTTON_LEFT) &&
+          Vector2DistanceSqr(point.pos, GetMousePosition()) <
+              point_radius * point_radius) {
+        grabbed_point = &point;
+        grabbed_offset = Vector2Subtract(point.pos, GetMousePosition());
+      }
+    }
+    if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT)) {
+      grabbed_point = nullptr;
+    }
+
+    if (IsMouseButtonDown(MOUSE_BUTTON_LEFT) && grabbed_point) {
+      grabbed_point->pos = Vector2Add(GetMousePosition(), grabbed_offset);
+    }
+
+    points_links_mutex.unlock();
+
     BeginDrawing();
 
     ClearBackground(RAYWHITE);
 
-    points_links_mutex.lock();
+    points_links_mutex.unlock();
 
     for (const Link &link : links)
-      DrawLineEx(points[link.ia].pos, points[link.ib].pos, 12.0f, GRAY);
+      DrawLineEx(points[link.ia].pos, points[link.ib].pos, link_width, GRAY);
 
     for (const Point &point : points) {
       Color color = BLACK;
-      switch(point.state) {
-        case Point::State::Free:
-          color = DARKGRAY;
-          break;
-        case Point::State::Locked:
-          color = RED;
-          break;
-        case Point::State::Animated:
-          color = BLUE;
-          break;
+      switch (point.state) {
+      case Point::State::Free:
+        color = DARKGRAY;
+        break;
+      case Point::State::Locked:
+        color = RED;
+        break;
+      case Point::State::Animated:
+        color = BLUE;
+        break;
       }
-      DrawCircleV(point.pos, 18.0f, color);
+      DrawCircleV(point.pos, point_radius, color);
     }
 
     points_links_mutex.unlock();
 
     DrawText(
-        fmt::format("Frame time: {:.2f} ms", GetFrameTime() * 1000.0f)
-            .c_str(),
+        fmt::format("Frame time: {:.2f} ms", GetFrameTime() * 1000.0f).c_str(),
         10, 10, font_size, BLACK);
 
-    DrawText(fmt::format("Frames per second: {}", GetFPS()).c_str(), 10,
-                 30, font_size, BLACK);
+    DrawText(fmt::format("Frames per second: {}", GetFPS()).c_str(), 10, 30,
+             font_size, BLACK);
 
-    DrawText(
-        fmt::format("Physics time: {:.2f} ms", physics_delta_time).c_str(), 10,
-        60, font_size, BLACK);
+    DrawText(fmt::format("Physics time: {:.2f} ms", physics_delta_time).c_str(),
+             10, 60, font_size, BLACK);
 
     DrawText(fmt::format("Physics per second: {}",
-                             (int)(1000.0f / physics_delta_time))
-                     .c_str(),
-                 10, 80, font_size, BLACK);
+                         (int)(1000.0f / physics_delta_time))
+                 .c_str(),
+             10, 80, font_size, BLACK);
 
     if (paused)
       DrawText("paused", 10, window_height - 30, font_size, BLACK);
@@ -190,7 +211,7 @@ int main() {
                                        orig_index + 1, ropes.size());
     int name_length = MeasureText(name_str.c_str(), font_size);
     DrawText(name_str.c_str(), window_width - name_length - 10,
-                 window_height - 30, font_size, BLACK);
+             window_height - 30, font_size, BLACK);
 
     EndDrawing();
 
